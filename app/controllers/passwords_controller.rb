5f1071c853feda6584e20b74a9c35ed80b8e7f10
Z313PH <eliewamana10@gmail.com>
2025-04-12 20:09:01 -0600

    added password strength score feature

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -13,9 +13,21 @@ class PasswordsController < ApplicationController
     charset += ('A'..'Z').to_a if use_upper
     charset += ('0'..'9').to_a if use_numbers
     charset += %w[! @ # $ % ^ & *] if use_special
-  
+
     password = Array.new(length) { charset.sample }.join
-    redirect_to root_path(password: password)
+    strength_score = 0
+    strength_score += 1 if password.length >= 8
+    strength_score += 1 if password.match(/[A-Z]/)
+    strength_score += 1 if password.match(/[0-9]/)
+    strength_score += 1 if password.match(/[^A-Za-z0-9]/)
+
+    @strength = case strength_score
+            when 0..1 then "Weak"
+            when 2 then "Moderate"
+            when 3 then "Strong"
+            else "Very Strong"
+            end
+    redirect_to root_path(password: password, strength: @strength)
   end
 end
 # This controller handles the password generation logic. It has two actions:
